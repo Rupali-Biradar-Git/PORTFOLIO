d020f7305fa574c9b5602c8bde6c8ecc75a32e81
Rupali-Biradar-Git <rupalidb200596@gmail.com>
2026-02-05 16:32:52 +0530

    Add all portfolio files

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// $(document).ready(function () {
+//     $('.test-person').owlCarousel({
+//         margin: 20,
+//         loop: true,
+//         nav: false,
+//         autoplay: true,
+//         autoplayHoverPause: true,
+//         responsive: {
+//             0: {
+//                 items: 1
+//             },
+//             768: {
+//                 items: 2
+//             },
+//             992: {
+//                 items: 3
+//             },
+//             1200: {
+//                 items: 4
+//             }
+//         }
+//     })
+// })
+
+
